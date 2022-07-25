e3bb735a83b721a837b0f9feead46dda17302812
HONGSB14 <sbin0114@naver.com>
2022-07-25 18:04:11 +0900

    503 : data

--- src/main/resources/static/js/data/main.js
+++ src/main/resources/static/js/data/main.js
@@ -35,38 +35,26 @@ function map(){
     imageSize = new kakao.maps.Size(30, 39), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-	// 마커가 표시될 위치입니다
-	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-	    markerPosition  = new kakao.maps.LatLng();
-
-	// 마커를 생성합니다
-
-	var marker = new kakao.maps.Marker({
-	    position: markerPosition,
-  		image: markerImage,
-  		 clickable: true
-	});
-
-
-	marker.setMap(map);
 
 	///////////////////////////////////////////////////////////버스 위치 ////////////////////////////////////////////
 
 
 	setInterval(function(){
 		//마커 제거
-    		for (var i = 0; i < markers.length; i++) {
+    	for (var i = 0; i < markers.length; i++) {
     	        markers[i].setMap(null);
-    	        markers.splice(1,i);
-    	    }
-    	    console.log(markers);
-	//차량 번호 만큼 반복문 (다수 차량 지도 표시를 위해 )
+    	}
+        markers = [];
+       console.log(markers);
 		$.ajax({
 			url:"/car/carInfo",
 			method:"GET",
 			data:{"companyNumber":session},
 			contentType:"application/json; charset:UTF-8",
 			success:function(data){
+
+
+
 			console.log(data);
 				if(data){
 		            for(let i=0; i<data.length; i++){
@@ -74,7 +62,7 @@ function map(){
 		                	//데이터에서 좌표 불러오기  (타코미터 저장용)
                         	tmX=data[i].tmX;
                         	tmY=data[i].tmY;
-                            plainNo=data[i].plainNo;   //***********************한글 인코딩  오류 ( 수정필요!!!!!!)
+                            plainNo=data[i].plainNo;   //***********************한글 인코딩  오류 ( 인텔리 제이 오류)
                             console.log(plainNo);
                             //좌표를 먼저 합치고 형태:( 736733673,13551331)
                             let coordinate=tmY+","+tmX;
@@ -92,22 +80,19 @@ function map(){
 //                                                    }
 //                                                }
 //                                        });
-		           }
-
-					//마커 생성
-					maker= new kakao.maps.Marker ({
-
-							position: new kakao.maps.LatLng(tmY,tmX),
+	                //마커 생성
+					var marker= new kakao.maps.Marker ({
+							position: new kakao.maps.LatLng(data[i].tmY,data[i].tmX),
 							image: new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
 							map:map
 					});
 					//마커를 배열에 저장
                     markers.push(marker);
+		           }
 				}else{
 					console.log("에러가 났어요~!");
 				}
 			}
 		});
-	},10000);
-
+	},5000);
 }
